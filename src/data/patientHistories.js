e0dfe143d98d7feb67733b8a39e392ef2cf5a576
ricazaldiviaaa <bc.ricamay.zaldivia@cvsu.edu.ph>
2026-01-20 10:57:28 +0800

    Data

--- src/data/patientHistories.js
+++ src/data/patientHistories.js
@@ -29,4 +29,34 @@ export const patientHistories = [
       },
     ],
   },
+  {
+    patientEmail: "john@example.com",
+    records: [
+      {
+        date: "2025-11-05",
+        diagnosis: "Allergy",
+        prescriptions: [
+          {
+            brand: "Antihistamine",
+            strengthValue: 10,
+            strengthUnit: "mg",
+            dosage: "Tablet",
+            durationValue: 7,
+            durationUnit: "days",
+            frequency: 1
+          },
+          {
+            brand: "Nasal Spray",
+            strengthValue: 2,
+            strengthUnit: "ml",
+            dosage: "Spray",
+            durationValue: 5,
+            durationUnit: "days",
+            frequency: 2
+          }
+        ],
+        doctorEmail: "sarah@example.com",
+      },
+    ],
+  },
 ];
